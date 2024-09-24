96c0240078b567f9c8712e09f9ddf534268207e5
Aman yadav <amanyadav9675maa@gmail.com>
2024-09-24 23:09:38 +0530

    First day of project

---
+++ src/main/java/bank/app/BankPortalApplication.java
@@ -0,0 +1,17 @@
+package bank.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "bank.app")
+public class BankPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankPortalApplication.class, args);
+	}
+
+}
---
+++ src/main/java/bank/app/controllers/LoginController.java
@@ -0,0 +1,23 @@
+package bank.app.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class LoginController {
+
+	@GetMapping("/login/user")
+	public String LoginPage() {
+		System.out.println("Login called()");
+		return "login";
+	}
+	
+	@GetMapping("/register")
+	public String openUserRegisterationPage() {
+		
+		return "user_registration";
+	}
+}
